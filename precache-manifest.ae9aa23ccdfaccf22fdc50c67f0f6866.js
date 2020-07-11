7b9d9731f717d1c344c78b3f4a1972e5a52ad849
Almaz <atunstyle@gmail.com>
2020-07-11 15:46:55 +0300

    Updates

---
+++ precache-manifest.ae9aa23ccdfaccf22fdc50c67f0f6866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e85eb451a750245bba63d561a864d8d",
+    "url": "/Score_board/index.html"
+  },
+  {
+    "revision": "98d5d076d1bc9e955edc",
+    "url": "/Score_board/static/css/main.ca694f69.chunk.css"
+  },
+  {
+    "revision": "a89098e37d20c8e82984",
+    "url": "/Score_board/static/js/2.6d9e0e4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Score_board/static/js/2.6d9e0e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d5d076d1bc9e955edc",
+    "url": "/Score_board/static/js/main.e2daaffa.chunk.js"
+  },
+  {
+    "revision": "e6c17148ed6962e82684",
+    "url": "/Score_board/static/js/runtime-main.b82f45d0.js"
+  }
+]);
